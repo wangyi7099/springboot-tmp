ee2e73edabeedbd45bc7874722186199412281f6
this.getName() <395471046@qq.com>
2019-04-14 17:36:38 +0800

    部门api

---
+++ src/main/java/cn/stylefeng/guns/modular/frontapi/controller/DeptApiController.java
@@ -0,0 +1,172 @@
+package cn.stylefeng.guns.modular.frontapi.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.stylefeng.guns.core.common.annotion.BussinessLog;
+import cn.stylefeng.guns.core.common.constant.dictmap.DeptDict;
+import cn.stylefeng.guns.core.common.constant.factory.ConstantFactory;
+import cn.stylefeng.guns.core.common.node.TreeviewNode;
+import cn.stylefeng.guns.core.common.node.ZTreeNode;
+import cn.stylefeng.guns.core.log.LogObjectHolder;
+import cn.stylefeng.guns.modular.system.entity.Dept;
+import cn.stylefeng.guns.modular.system.model.DeptDto;
+import cn.stylefeng.guns.modular.system.service.DeptService;
+import cn.stylefeng.guns.modular.system.warpper.DeptTreeWrapper;
+import cn.stylefeng.guns.modular.system.warpper.DeptWrapper;
+import cn.stylefeng.roses.core.reqres.response.ResponseData;
+import cn.stylefeng.roses.core.treebuild.DefaultTreeBuildFactory;
+import cn.stylefeng.roses.core.util.ToolUtil;
+import cn.stylefeng.roses.kernel.model.exception.RequestEmptyException;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/gunsApi/dept")
+public class DeptApiController {
+
+    @Autowired
+    private DeptService deptService;
+
+    /**
+     * 跳转到修改部门
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:56 PM
+     */
+
+    @RequestMapping("/dept_update")
+    public ResponseData deptUpdate(@RequestParam("deptId") Long deptId) {
+
+        if (ToolUtil.isEmpty(deptId)) {
+            throw new RequestEmptyException();
+        }
+
+        //缓存部门修改前详细信息
+        Dept dept = deptService.getById(deptId);
+        LogObjectHolder.me().set(dept);
+
+        return ResponseData.success();
+    }
+
+    /**
+     * 获取部门的tree列表，ztree格式
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:56 PM
+     */
+    @RequestMapping(value = "/tree")
+    @ResponseBody
+    public ResponseData tree() {
+        List<ZTreeNode> tree = this.deptService.tree();
+        tree.add(ZTreeNode.createParent());
+        return ResponseData.success(tree);
+    }
+
+    /**
+     * 获取部门的tree列表，treeview格式
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:57 PM
+     */
+    @RequestMapping(value = "/treeview")
+    @ResponseBody
+    public ResponseData treeview() {
+        List<TreeviewNode> treeviewNodes = this.deptService.treeviewNodes();
+
+        //构建树
+        DefaultTreeBuildFactory<TreeviewNode> factory = new DefaultTreeBuildFactory<>();
+        factory.setRootParentId("0");
+        List<TreeviewNode> results = factory.doTreeBuild(treeviewNodes);
+
+        //把子节点为空的设为null
+        DeptTreeWrapper.clearNull(results);
+
+        return ResponseData.success(results);
+    }
+
+    /**
+     * 新增部门
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:57 PM
+     */
+    @BussinessLog(value = "添加部门", key = "simpleName", dict = DeptDict.class)
+    @RequestMapping(value = "/add")
+    @ResponseBody
+    public ResponseData add(Dept dept) {
+        this.deptService.addDept(dept);
+        return ResponseData.success();
+    }
+
+    /**
+     * 获取所有部门列表
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:57 PM
+     */
+    @RequestMapping(value = "/list")
+    @ResponseBody
+    public ResponseData list(@RequestParam(value = "condition", required = false) String condition,
+                             @RequestParam(value = "deptId", required = false) Long deptId) {
+        Page<Map<String, Object>> list = this.deptService.list(condition, deptId);
+        Page<Map<String, Object>> wrap = new DeptWrapper(list).wrap();
+        return ResponseData.success(wrap);
+    }
+
+    /**
+     * 部门详情
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:57 PM
+     */
+    @RequestMapping(value = "/detail/{deptId}")
+    @ResponseBody
+    public ResponseData detail(@PathVariable("deptId") Long deptId) {
+        Dept dept = deptService.getById(deptId);
+        DeptDto deptDto = new DeptDto();
+        BeanUtil.copyProperties(dept, deptDto);
+        deptDto.setPName(ConstantFactory.me().getDeptName(deptDto.getPid()));
+        return ResponseData.success(deptDto);
+    }
+
+    /**
+     * 修改部门
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:57 PM
+     */
+    @BussinessLog(value = "修改部门", key = "simpleName", dict = DeptDict.class)
+    @RequestMapping(value = "/update")
+    @ResponseBody
+    public ResponseData update(Dept dept) {
+        deptService.editDept(dept);
+        return ResponseData.success();
+    }
+
+    /**
+     * 删除部门
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:57 PM
+     */
+    @BussinessLog(value = "删除部门", key = "deptId", dict = DeptDict.class)
+    @RequestMapping(value = "/delete")
+    @ResponseBody
+    public ResponseData delete(@RequestParam Long deptId) {
+
+        //缓存被删除的部门名称
+        LogObjectHolder.me().set(ConstantFactory.me().getDeptName(deptId));
+
+        deptService.deleteDept(deptId);
+
+        return ResponseData.success();
+    }
+
+}
