c1575309d4a5524ae4e2e69e1d6bdf06340b0fb3
this.getName() <395471046@qq.com>
2019-04-27 20:17:35 +0800

    画布

---
+++ src/main/java/cn/stylefeng/guns/modular/demos/controller/flowableController.java
@@ -0,0 +1,34 @@
+package cn.stylefeng.guns.modular.demos.controller;
+
+import org.flowable.ui.common.model.UserRepresentation;
+import org.flowable.ui.common.security.DefaultPrivileges;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/assets/modeler/app")
+public class flowableController {
+
+    @RequestMapping(value = "/rest/account", method = RequestMethod.GET, produces = "application/json")
+    public UserRepresentation getAccount() {
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setId("admin");
+        userRepresentation.setEmail("admin@flowable.org");
+        userRepresentation.setFullName("Test Administrator");
+        userRepresentation.setLastName("Administrator");
+        userRepresentation.setFirstName("Test");
+        List<String> privileges = new ArrayList<>();
+        privileges.add(DefaultPrivileges.ACCESS_MODELER);
+        privileges.add(DefaultPrivileges.ACCESS_IDM);
+        privileges.add(DefaultPrivileges.ACCESS_ADMIN);
+        privileges.add(DefaultPrivileges.ACCESS_TASK);
+        privileges.add(DefaultPrivileges.ACCESS_REST_API);
+        userRepresentation.setPrivileges(privileges);
+        return userRepresentation;
+    }
+
+}
